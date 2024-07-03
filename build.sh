ed9a0dcb2587acc881c6b6dd69f49a9e54aa3ef1
hesam <bhesam2000@yahoo.com>
2024-07-04 00:33:53 +0430

    edit script

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 cd ./client 
 git checkout dockerize
-git pull
+git pull origin master
 
 cd ../server
 git checkout dockerize
-git pull
+git pull origin master
 
 sudo docker compose up --build
